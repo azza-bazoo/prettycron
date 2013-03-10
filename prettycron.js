701f957d1db4f7069b1d30be5d5bc59ce0fd3502
Hourann Bosci <hourannb@dview.net>
2013-03-10 16:53:01 +0800

    reindent everything at :set tabstop=2 (yes, I'm crazy)

--- prettycron.js
+++ prettycron.js
@@ -24,9 +24,9 @@
 (function() {
 
   var printLoop = function(pArray) {
-          if (pArray.length < 2) { return moment()._lang.ordinal(pArray); }
-          var lastE = pArray.pop();
-          return pArray.join(', ') + ' and ' + moment()._lang.ordinal(lastE);
+    if (pArray.length < 2) { return moment()._lang.ordinal(pArray); }
+    var lastE = pArray.pop();
+    return pArray.join(', ') + ' and ' + moment()._lang.ordinal(lastE);
   };
 
   var formatDay = function(value, type) {
@@ -38,74 +38,74 @@
   };
 
   var printDateLoop = function(pArray, type) {
-          if (pArray.length < 2) { return formatDay(''+pArray[0], type); }
-          var lastE = '' + pArray.pop();
+    if (pArray.length < 2) { return formatDay(''+pArray[0], type); }
+    var lastE = '' + pArray.pop();
   
-          var retString = '';
-          for (p in pArray) {
-                  if (retString.lenght > 0) {
-                          retString+= ', ';
-                  }
-                  retString+= formatDay(p, type);
-          }
-          return retString + ' and ' + formatDay(lastE, type);
+    var retString = '';
+    for (p in pArray) {
+      if (retString.lenght > 0) {
+        retString+= ', ';
+      }
+      retString+= formatDay(p, type);
+    }
+    return retString + ' and ' + formatDay(lastE, type);
   };
 
   // both Moment and Later have zero-fill functions, but they're private
   var zeroPad = function(x) {
-          return (x < 10)? '0' + x : x;
+    return (x < 10)? '0' + x : x;
   };
 
   var toString = function(schedule) {
-          var hmText = 'Every ';
-          // If max two of h or m, print in HH:MM format
-          if (schedule['h'] && schedule['m'] && schedule['h'].length <= 2 && schedule['m'].length <= 2) {
-                  var hm = [];
-                  for (h in schedule['h']) {
-                          for (m in schedule['m']) {
-                                  hm.push(zeroPad(h) + ':' + zeroPad(m));
-                          }
-                  }
-                  if (hm.length < 2) {
-                          hmText = hm[0];
-                  } else {
-                          var lastE = hm.pop();
-                          hmText = hm.join(', ') + ' and ' + lastE;
-                  }
-          } else {
-                  if(schedule['h']) { // Hour
-                          if (schedule['m']) { // Got specific minutes
-                                  hmText+= printLoop(schedule['m']) + ' minute past the ' + printLoop(schedule['h']) + ' hour';
-                          } else { // Every minute
-                                  hmText+= 'minute of ' + printLoop(schedule['h']) + ' hour';
-                          }
-                  } else if(schedule['m']) { // Minute
-                          if (schedule['m'].length == 1 && schedule['m'][0] == 0) {
-                                  hmText+= 'hour, on the hour';
-                          } else {
-                                  hmText+= printLoop(schedule['m']) + ' minute past every hour';
-                          }
-                  } else { // * for both Hour and Minute
-                          hmText+= 'minute';
-                  }
-          }
-  
-          if (schedule['D']) { // Day of month
-                  hmText+= ' on the ' + printLoop(schedule['D']);
-                  if (!schedule['M']) {
-                          hmText+= ' every month';
-                  }
-          }
-  
-          if (schedule['M']) { // Month
-                  hmText+= ' in ' + printDateLoop(schedule['M'], 'mon');
-          }
-  
-          if (schedule['d']) { // Day of week
-                  hmText+= ' on ' + printDateLoop(schedule['d'], 'dow');
-          }
-  
-          return hmText;
+    var hmText = 'Every ';
+    // If max two of h or m, print in HH:MM format
+    if (schedule['h'] && schedule['m'] && schedule['h'].length <= 2 && schedule['m'].length <= 2) {
+      var hm = [];
+      for (h in schedule['h']) {
+        for (m in schedule['m']) {
+          hm.push(zeroPad(h) + ':' + zeroPad(m));
+        }
+      }
+      if (hm.length < 2) {
+        hmText = hm[0];
+      } else {
+        var lastE = hm.pop();
+        hmText = hm.join(', ') + ' and ' + lastE;
+      }
+    } else {
+      if(schedule['h']) { // Hour
+        if (schedule['m']) { // Got specific minutes
+          hmText+= printLoop(schedule['m']) + ' minute past the ' + printLoop(schedule['h']) + ' hour';
+        } else { // Every minute
+          hmText+= 'minute of ' + printLoop(schedule['h']) + ' hour';
+        }
+      } else if(schedule['m']) { // Minute
+        if (schedule['m'].length == 1 && schedule['m'][0] == 0) {
+          hmText+= 'hour, on the hour';
+        } else {
+          hmText+= printLoop(schedule['m']) + ' minute past every hour';
+        }
+      } else { // * for both Hour and Minute
+        hmText+= 'minute';
+      }
+    }
+    
+    if (schedule['D']) { // Day of month
+      hmText+= ' on the ' + printLoop(schedule['D']);
+      if (!schedule['M']) {
+        hmText+= ' every month';
+      }
+    }
+    
+    if (schedule['M']) { // Month
+      hmText+= ' in ' + printDateLoop(schedule['M'], 'mon');
+    }
+    
+    if (schedule['d']) { // Day of week
+      hmText+= ' on ' + printDateLoop(schedule['d'], 'dow');
+    }
+    
+    return hmText;
   };
 
   // attach ourselves to window in the browser, and to exports in Node  
@@ -116,4 +116,3 @@
   };
 
 }).call(this);
-
